16b4db2201d5df863b43696ee28eeae79e4d4eb4
isacvale <isac@ocastudios.com>
2019-02-27 12:56:55 -0300

    Fixed bugcaused by excessive calls in a single command.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function formatQuery( query, data ){
+function formatBatch( query, data ){
   let match = query.match(/\(([^\(]*)\);?$/)
   let params = match[1].split(',').map(x=>x.trim())
   let response = []
@@ -16,5 +16,23 @@ function formatQuery( query, data ){
     response.push(`(${args.join(',')})`)
   })
   return query.replace(match[0],'')+response.join(',')
+}
+
+function formatQuery( query, data ){
+  let batches = data.reduce( (acc, cur, ind, src) => {
+    let fit = 0
+    acc.forEach( batch => {
+      if(batch.length<maxRows){
+        batch.push(cur)
+        fit = 1
+      }
+    })
+    if(!fit)
+      acc.push([cur])
+    return acc
+  },[[]])
+  let queries = batches.map( batch => formatBatch( query, batch ))
+  let finalQuery = queries.join(';')
+  return finalQuery
 }
 module.exports = formatQuery
